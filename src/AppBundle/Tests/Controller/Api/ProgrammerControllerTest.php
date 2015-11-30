9ffe9c042933d45f93bee0077fdbb670104c4e8c
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:25 -0500

    Testing: Asserting specific username

--- src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
+++ src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
@@ -7,9 +7,8 @@ class ProgrammerControllerTest extends ApiTestCase
 {
     public function testPOST()
     {
-        $nickname = 'ObjectOrienter'.rand(0, 999);
         $data = array(
-            'nickname' => $nickname,
+            'nickname' => 'ObjectOrienter',
             'avatarNumber' => 5,
             'tagLine' => 'a test dev!'
         );
@@ -21,7 +20,9 @@ class ProgrammerControllerTest extends ApiTestCase
 
         $this->assertEquals(201, $response->getStatusCode());
         $this->assertTrue($response->hasHeader('Location'));
+        $this->assertEquals('/api/programmers/ObjectOrienter', $response->getHeader('Location'));
         $finishedData = json_decode($response->getBody(true), true);
         $this->assertArrayHasKey('nickname', $finishedData);
+        $this->assertEquals('ObjectOrienter', $finishedData['nickname']);
     }
 }
