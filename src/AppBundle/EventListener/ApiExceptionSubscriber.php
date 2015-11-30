b9aa96efc8f22f4d1be28307ddb56fec7eaf093f
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:39 -0500

    Errors: Making type a URL

--- src/AppBundle/EventListener/ApiExceptionSubscriber.php
+++ src/AppBundle/EventListener/ApiExceptionSubscriber.php
@@ -50,8 +50,14 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
             }
         }
 
+        $data = $apiProblem->toArray();
+        // making type a URL, to a temporarily fake page
+        if ($data['type'] != 'about:blank') {
+            $data['type'] = 'http://localhost:8000/docs/errors#'.$data['type'];
+        }
+
         $response = new JsonResponse(
-            $apiProblem->toArray(),
+            $data,
             $apiProblem->getStatusCode()
         );
         $response->headers->set('Content-Type', 'application/problem+json');
--- src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
+++ src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
@@ -160,7 +160,7 @@ EOF;
         ]);
 
         $this->assertEquals(400, $response->getStatusCode());
-        $this->asserter()->assertResponsePropertyEquals($response, 'type', 'invalid_body_format');
+        $this->asserter()->assertResponsePropertyContains($response, 'type', 'invalid_body_format');
     }
 
     public function test404Exception()
