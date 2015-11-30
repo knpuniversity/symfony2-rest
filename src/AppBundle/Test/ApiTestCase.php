dfa26cc52a5c3eb0d93f1c1369a6cabf8a7a2f00
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:25 -0500

    Testing: Debug on errors

--- src/AppBundle/Test/ApiTestCase.php
+++ src/AppBundle/Test/ApiTestCase.php
@@ -3,17 +3,34 @@
 namespace AppBundle\Test;
 
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Message\AbstractMessage;
+use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\Subscriber\History;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ApiTestCase extends KernelTestCase
 {
     private static $staticClient;
 
     /**
+     * @var History
+     */
+    private static $history;
+
+    /**
      * @var Client
      */
     protected $client;
+
+    /**
+     * @var FormatterHelper
+     */
+    private $formatterHelper;
 
     public static function setUpBeforeClass()
     {
@@ -23,6 +40,9 @@ class ApiTestCase extends KernelTestCase
                 'exceptions' => false
             ]
         ]);
+        self::$history = new History();
+        self::$staticClient->getEmitter()
+            ->attach(self::$history);
 
         self::bootKernel();
     }
@@ -42,6 +62,20 @@ class ApiTestCase extends KernelTestCase
         // purposefully not calling parent class, which shuts down the kernel
     }
 
+    protected function onNotSuccessfulTest(Exception $e)
+    {
+        if (self::$history && $lastResponse = self::$history->getLastResponse()) {
+            $this->printDebug('');
+            $this->printDebug('<error>Failure!</error> when making the following request:');
+            $this->printLastRequestUrl();
+            $this->printDebug('');
+
+            $this->debugResponse($lastResponse);
+        }
+
+        throw $e;
+    }
+
     private function purgeDatabase()
     {
         $purger = new ORMPurger($this->getService('doctrine')->getManager());
@@ -52,5 +86,114 @@ class ApiTestCase extends KernelTestCase
     {
         return self::$kernel->getContainer()
             ->get($id);
+    }
+
+    protected function printLastRequestUrl()
+    {
+        $lastRequest = self::$history->getLastRequest();
+
+        if ($lastRequest) {
+            $this->printDebug(sprintf('<comment>%s</comment>: <info>%s</info>', $lastRequest->getMethod(), $lastRequest->getUrl()));
+        } else {
+            $this->printDebug('No request was made.');
+        }
+    }
+
+    protected function debugResponse(ResponseInterface $response)
+    {
+        $this->printDebug(AbstractMessage::getStartLineAndHeaders($response));
+        $body = (string) $response->getBody();
+
+        $contentType = $response->getHeader('Content-Type');
+        if ($contentType == 'application/json' || strpos($contentType, '+json') !== false) {
+            $data = json_decode($body);
+            if ($data === null) {
+                // invalid JSON!
+                $this->printDebug($body);
+            } else {
+                // valid JSON, print it pretty
+                $this->printDebug(json_encode($data, JSON_PRETTY_PRINT));
+            }
+        } else {
+            // the response is HTML - see if we should print all of it or some of it
+            $isValidHtml = strpos($body, '</body>') !== false;
+
+            if ($isValidHtml) {
+                $this->printDebug('');
+                $crawler = new Crawler($body);
+
+                // very specific to Symfony's error page
+                $isError = $crawler->filter('#traces-0')->count() > 0
+                    || strpos($body, 'looks like something went wrong') !== false;
+                if ($isError) {
+                    $this->printDebug('There was an Error!!!!');
+                    $this->printDebug('');
+                } else {
+                    $this->printDebug('HTML Summary (h1 and h2):');
+                }
+
+                // finds the h1 and h2 tags and prints them only
+                foreach ($crawler->filter('h1, h2')->extract(array('_text')) as $header) {
+                    // avoid these meaningless headers
+                    if (strpos($header, 'Stack Trace') !== false) {
+                        continue;
+                    }
+                    if (strpos($header, 'Logs') !== false) {
+                        continue;
+                    }
+
+                    // remove line breaks so the message looks nice
+                    $header = str_replace("\n", ' ', trim($header));
+                    // trim any excess whitespace "foo   bar" => "foo bar"
+                    $header = preg_replace('/(\s)+/', ' ', $header);
+
+                    if ($isError) {
+                        $this->printErrorBlock($header);
+                    } else {
+                        $this->printDebug($header);
+                    }
+                }
+
+                $profilerUrl = $response->getHeader('X-Debug-Token-Link');
+                if ($profilerUrl) {
+                    $fullProfilerUrl = $response->getHeader('Host').$profilerUrl;
+                    $this->printDebug('');
+                    $this->printDebug(sprintf(
+                        'Profiler URL: <comment>%s</comment>',
+                        $fullProfilerUrl
+                    ));
+                }
+
+                // an extra line for spacing
+                $this->printDebug('');
+            } else {
+                $this->printDebug($body);
+            }
+        }
+    }
+
+    /**
+     * Print a message out - useful for debugging
+     *
+     * @param $string
+     */
+    protected function printDebug($string)
+    {
+        echo $string."\n";
+    }
+
+    /**
+     * Print a debugging message out in a big red block
+     *
+     * @param $string
+     */
+    protected function printErrorBlock($string)
+    {
+        if ($this->formatterHelper === null) {
+            $this->formatterHelper = new FormatterHelper();
+        }
+        $output = $this->formatterHelper->formatBlock($string, 'bg=red;fg=white', true);
+
+        $this->printDebug($output);
     }
 }
