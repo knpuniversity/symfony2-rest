d67f821d5f01cafdab40831a5b4a16cbac608ebe
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:20 -0500

    POST Programmer: Creating and saving entity

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -2,13 +2,14 @@
 
 namespace AppBundle\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Programmer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProgrammerController extends Controller
+class ProgrammerController extends BaseController
 {
     /**
      * @Route("/api/programmers")
@@ -16,8 +17,16 @@ class ProgrammerController extends Controller
      */
     public function newAction(Request $request)
     {
-        $data = $request->getContent();
+        $data = json_decode($request->getContent(), true);
 
-        return new Response($data);
+        $programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+        $programmer->setTagLine($data['tagLine']);
+        $programmer->setUser($this->findUserByUsername('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        return new Response('It worked. Believe me - I\'m an API');
     }
 }
