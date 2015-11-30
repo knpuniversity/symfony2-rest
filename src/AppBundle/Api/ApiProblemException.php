66333e9b63e454bdfba31e005643a67539d7869a
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:35 -0500

    Errors: ApiProblemException ApiProblem arg

--- src/AppBundle/Api/ApiProblemException.php
+++ src/AppBundle/Api/ApiProblemException.php
@@ -6,4 +6,12 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ApiProblemException extends HttpException
 {
+    private $apiProblem;
+
+    public function __construct(ApiProblem $apiProblem, $statusCode, $message = null, \Exception $previous = null, array $headers = array(), $code = 0)
+    {
+        $this->apiProblem = $apiProblem;
+
+        parent::__construct($statusCode, $message, $previous, $headers, $code);
+    }
 }
