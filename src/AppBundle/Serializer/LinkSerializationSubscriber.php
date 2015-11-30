3fb4c96e7ca7f9b2f4029ff28e952ae1becda02e
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:42 -0500

    Serialization: Create subscriber

---
+++ src/AppBundle/Serializer/LinkSerializationSubscriber.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle\Serializer;
+
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+
+class LinkSerializationSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+    }
+}
