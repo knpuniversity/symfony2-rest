2159ecdca28c3394bce329cc768e492e9190b6ca
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:24 -0500

    Testing: Adding asserts to PHPUnit test

--- src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
+++ src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
@@ -23,5 +23,10 @@ class ProgrammerControllerTest extends \PHPUnit_Framework_TestCase
         $response = $client->post('/api/programmers', [
             'body' => json_encode($data)
         ]);
+
+        $this->assertEquals(201, $response->getStatusCode());
+        $this->assertTrue($response->hasHeader('Location'));
+        $finishedData = json_decode($response->getBody(true), true);
+        $this->assertArrayHasKey('nickname', $finishedData);
     }
 }
