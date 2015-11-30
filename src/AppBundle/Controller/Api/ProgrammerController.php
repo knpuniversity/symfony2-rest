f3d32b22e04c267a7c76951ac2c9ee5d0dcaae97
Ryan Weaver <ryan@thatsquality.com>
2015-11-30 10:15:21 -0500

    POST Programmer: 201 status code

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -30,6 +30,6 @@ class ProgrammerController extends BaseController
         $em->persist($programmer);
         $em->flush();
 
-        return new Response('It worked. Believe me - I\'m an API');
+        return new Response('It worked. Believe me - I\'m an API', 201);
     }
 }
